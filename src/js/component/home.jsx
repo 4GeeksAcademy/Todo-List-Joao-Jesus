0e6195c1798314aabef3cb7149d21ad4dd3c5e01
joaoaugusto-jesus <147534481+joaoaugusto-jesus@users.noreply.github.com>
2024-03-12 16:25:03 +0000

    Functions are working

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -9,31 +9,50 @@ const AnyComponent = () => {
 	const [tasks, setTasks] = useState([]);
     const [inputValue, setInputValue] = useState('');
 
-    // Function to add a new task to the list
-    const addTask = () => {
+    // Function to add a new task to todo list
+    	const addTask = () => {
         if (inputValue.trim() !== '') {
             setTasks([...tasks, inputValue]);
             setInputValue('');
+        } 
+    };
+
+	// Function to add eventhandler to the Key Enter
+	const handleKeyEnter = (e) => {
+        if (e.key === 'Enter') {
+            addTask();
         }
     };
-	const removeTask = (index) => {
+
+		// Function to remove a new task to todo list
+		const removeTask = (index) => {
         const updatedTasks = [...tasks];
         updatedTasks.splice(index, 1);
         setTasks(updatedTasks);
     };
 		 
-		return <div>
-			<input type="text" onChange={e => setInputValue(e.target.value) } placeholder="Enter a new task" value={inputValue} />
-			<button onClick={addTask}>Click to validate empty</button>
-		
-		<ul>
-		{tasks.map((task, index) => (
-			<li key={index}>
-				{task}
-				<button onClick={() => removeTask(index)}>Remove</button>
-			</li>
-		))}
-	</ul></div>;
+		return <div className="container d-flex">
+			<input type="text" 
+			onChange={e => setInputValue(e.target.value)}  
+			onKeyPress={handleKeyEnter} 
+			placeholder="Enter a new task" 
+			value={inputValue} />
+			<button onClick={addTask}>Add a task</button>
+			
+			{tasks.length === 0 ? (
+                <p>No tasks, add a task</p>
+            ) : (
+                <ul>
+                    {tasks.map((task, index) => (
+                        <li key={index}>
+                            <p>{task}</p>
+                            <button onClick={() => removeTask(index)}>Remove task</button>
+                        </li>
+                    ))}
+                </ul>
+            )}
+        </div>
+    
 	}
 
 
